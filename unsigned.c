fb64c08828015dcc295552e25e8e9cb07c6d48a4
yuyabu <yuyabushita@gmail.com>
2018-07-09 12:26:32 +0900

    add how to use %u with unsigned int

--- unsigned.c
+++ unsigned.c
@@ -10,4 +10,10 @@ void main(){
 
 	printf("unsigned short 0xff is %d\n",us);//65536
 	printf("short 0xff is %d\n",s);//-1
+
+	//if print unsigned number, use%u
+	unsigned int ui = 3000000000;
+	
+	printf("int ui with %%d is %d\n",ui); //-1294967296
+	printf("int ui with %%u is %u\n",ui); //3000000000
 }
