b42b1a020ebd159edc56b5c220fbffec9d21d1c0
yuyabu <yuyabushita@gmail.com>
2018-07-09 19:28:06 +0900

    const test

--- chap2/const.c
+++ chap2/const.c
@@ -2,8 +2,14 @@
 
 
 void foo(char *p, const int length){
-	length =5;//error: cannot assign to variable 'length' with const-qualified type 'const int'
+//	length =5;//error: cannot assign to variable 'length' with const-qualified type 'const int'
+}
+
+void test(const char* memo){
+//	*memo = "change";//error: read-only variable is not assignable
+	
 }
 void main(){
+	test("test");
 
 }
