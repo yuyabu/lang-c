4bac482c768393a46f0446a9ac7f9ce332079d0d
yuyabu <yuyabushita@gmail.com>
2018-07-09 12:35:47 +0900

    char with %c,%d

---
+++ char.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+main(){
+	char c = 'a';
+	int num = 97;
+	
+	printf("char a with %%d is %d\n",c);//a
+	printf("char a with %%c is %c\n",c);//97
+	
+		
+	printf("int 97 with %%d is %d\n",num);//a
+	printf("int 97 with %%c is %c\n",num);//97
+}	
