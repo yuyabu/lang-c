f588f82778b08fad5da351fc1b751159a5f8ca3b
yuyabu <yuyabushita@gmail.com>
2018-07-09 12:39:14 +0900

    calc with char sample

--- char.c
+++ char.c
@@ -10,4 +10,10 @@ main(){
 		
 	printf("int 97 with %%d is %d\n",num);//a
 	printf("int 97 with %%c is %c\n",num);//97
+
+	//we can use char to calcrate
+	char a,b;
+	a=20;
+	b=6;
+	printf("%d\n",a*b);//120
 }	
