9926ccd4bc43a9ddb00ea3fd0b1cbb157c27133b
yuyabu <yuyabushita@gmail.com>
2018-07-09 20:09:44 +0900

    参照と参照先の保護の例を改善

--- chap2/const.c
+++ chap2/const.c
@@ -1,23 +1,46 @@
 #include<stdio.h>
 
+/**
+ * const char* で参照のみを保護する
+ * @param value [description]
+ */
+void referenceOnlyProtect(const char* p){
+	p = "test"; //書き換え可能。ただし呼び出し元に影響なし。
+	//ローカルスコープのvalueというポインタの値のみを書き換えている。
 
-void foo(char *p, const int length){
-//	length =5;//error: cannot assign to variable 'length' with const-qualified type 'const int'
-}
-
-void test(const char* memo){
-//	*memo = "change";//error: read-only variable is not assignable
-	
-}
-void test2(const char*memo){
-	memo = "change";
-	printf(memo);//change
+	//*p = 's';   //error: read-only variable is not assignable
+	//この変更ができてしまうと呼び出し元の値も変わってしまう。
 }
 
-void adressDefence(char *const memo){
-	memo = "change";//error: cannot assign to variable 'memo' with const-qualified type 'char *const'
+/**
+ * char const *pで値のみを保護する。
+ * 参照は書き換え可能
+ * @param p [description]
+ */
+void valueOnlyProtect(char* const p){
+	//p = "test";//error: cannot assign to variable 'p' with const-qualified type 'char *const'
+	//ポインタの値を"test"の参照で書き換えることができない。
+
+	//*p = 't';//Bus error: 10
+	//Busエラーという謎のエラーがでた。
+	//🚌プップ~
+	//この書き換えは本書的にはできるっぽいが。。。このエラーは謎。
+
 }
+
+void valueAndReferenceProtect(const char *const p){
+	//p="test";//error: cannot assign to variable 'p' with const-qualified type 'const char *const'
+	//参照の変更は許可されない
+
+	//*p = 't';//error: read-only variable is not assignable
+	//参照先の値も保護されている。
+}
+
 void main(){
+
+	//アドレスのみの保護
 	char *str = "test";
-	adressDefence(str);
+	valueOnlyProtect(str);
+	printf(str);
+
 }
