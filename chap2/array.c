695bdad6fba6d58447b09d43bd31e2f47b9b5b4c
yuyabu <yuyabushita@gmail.com>
2018-07-10 18:44:29 +0900

    add calc

--- chap2/array.c
+++ chap2/array.c
@@ -13,4 +13,31 @@ void main(){
     printf("%d ",n[i]);
     //0 1 2 3 4
   }
+
+  printf("\n--------\n");
+
+  for(int i=0;i<5;i++){
+    //配列はポインタなのでアドレスを使ってもアクセスできる
+    printf("%d ",*(n+i));
+    //0 1 2 3 4
+  }
+
+  printf("\n--------\n");
+
+  //配列のポインタに対する加算はそのバイト長だけアドレスが伸びる。
+  int i[5];
+  short s[5];
+
+  //intは4づつ伸びる
+  printf("i addr is %p\n",i);     //0x7ffee7fa6a90
+  printf("i+1 addr is %p\n",i+1); //0x7ffee7fa6a94
+
+  //shortは2づつ伸びる
+  printf("s addr is %p\n",s);     //0x7ffee878ea86
+  printf("s+1 addr is %p\n",s+1); //0x7ffee878ea88
+  
+
+
+
+
 }
