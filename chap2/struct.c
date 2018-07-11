b59e3bf91ea2a7fcd9aa0efc1355d4db03820e21
yuyabu <yuyabushita@gmail.com>
2018-07-11 12:23:35 +0900

    add some comment

--- chap2/struct.c
+++ chap2/struct.c
@@ -16,8 +16,8 @@ void main(){
 
   //char(1Byte)*15 + int(4Byte)で19Byteとなりそうだが、20が表示される。
   //これはワードアライメントと呼ばれるもので、構造体のサイズを偶数にするものである。
-  printf("struct Person size is %d\n",sizeof(struct Person));
-  printf("struct person size is %d\n",sizeof person);
+  printf("struct Person size is %d\n",sizeof(struct Person)); //20
+  printf("struct person size is %d\n",sizeof person);         //20
 
 
 
