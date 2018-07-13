088ef9aec6cbfd1f39992c98637141715bc773ad
yuyabu <yuyabushita@gmail.com>
2018-07-13 12:12:57 +0900

    add operater

---
+++ chap3/operator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void){
+    // Your code here!
+    int a =-1;
+    printf("%d\n",a);
+
+    char *judge_result = 0!= (a>0? a: 0)? "正の数":"0か負の数";
+
+    //奇妙にみえるが以下と同じ意味
+
+    //int fixed_number = a>0? a: 0;
+    //char *judge_result = 0!= fixed_number ? "正の数":"0か負の数";
+
+    printf("%s\n",judge_result);
+}
