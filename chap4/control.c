e695634016892298197a71290c73506c89c39e21
yuyabu <yuyabushita@gmail.com>
2018-07-13 19:23:43 +0900

    learn effective goto use case

--- chap4/control.c
+++ chap4/control.c
@@ -1,5 +1,29 @@
 #include<stdio.h>
 
+/**
+ * 九九を計算して表示する
+ * 引数limitの数字に到達した時点で処理をやめる
+ * @param limit [description]
+ */
+void nine_nine(int limit){
+  for(int i=1;i<10;i++){
+    for(int j=1;j<10;j++){
+
+      int result = i*j;
+
+      printf("%d\t",result);
+
+      if(result==limit){
+        goto last;
+      }
+    }
+    printf("\n");
+  }
+
+  last:
+  printf("\n");
+}
+
 int main(){
   printf("1\n");
 
@@ -13,5 +37,7 @@ int main(){
   //結果
   //1
   //3
+
+  nine_nine(64);
 
 }
