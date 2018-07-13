18e88c35fbe5d690f70ecf28f231bb914c42ac77
yuyabu <yuyabushita@gmail.com>
2018-07-13 20:17:53 +0900

    add c99 sample

---
+++ chap4/c99.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/*
+
+c99では肩の省略は許されていない。
+のでgccのオプションで-std=c99をつけるとコンパイルエラー。
+
+$ gcc -std=c99 chap4/c99.c
+chap4/c99.c:3:1: warning: type specifier missing, defaults to 'int'
+      [-Wimplicit-int]
+main(){
+^
+*/
+
+main(){
+}
