1d7140230ea8933d8f16160fd292193926cd647f
yuyabu <yuyabushita@gmail.com>
2018-07-15 14:13:05 +0900

    add free()

--- chap5/memory.c
+++ chap5/memory.c
@@ -15,4 +15,12 @@ int main(){
   puts(p);
   puts(vp);
 
+  free(p);
+  //free(vp);
+  // 間違えて、同じアドレスを指している変数を２回freeしてしまった。
+  // その時のエラーを記念に残しておく
+  // a.out(20725,0x7fffad00f380) malloc: *** error for object 0x7f8ebd402710: pointer being freed was not allocated
+  // *** set a breakpoint in malloc_error_break to debug
+  // Abort trap: 6
+
 }
