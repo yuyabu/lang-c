ab487fd979091e7702a5fbe9143b2e6607597cbf
yuyabu <yuyabushita@gmail.com>
2018-07-15 11:36:40 +0900

    add void args

--- other/special.c
+++ other/special.c
@@ -4,7 +4,8 @@
 void sub1(char *arg);
 void sub2(char arg[]);
 void sub3(char *arg[]);
-
+void sub4();
+void sub5(void);
 int main()
 {
     char str[] = {"1234"};
@@ -12,6 +13,25 @@ int main()
     sub2(str);
     char *pStr = str;
     sub3(&pStr);
+    sub4();
+    sub5();
+
+    sub4(1);
+    //sub5(1);//コンパイルエラー
+
+    // $ gcc -w -Wall other/special.c
+    // other/special.c:20:10: error: too many arguments to function call, expected 0,
+    //       have 1
+    //     sub5(1);
+    //     ~~~~ ^
+    // other/special.c:8:1: note: 'sub5' declared here
+    // void sub5(void);
+    // ^
+    // 1 error generated.
+
+
+
+
    return 0;
 }
 void sub1(char *arg)
@@ -41,4 +61,10 @@ void sub3(char **arg)
   printf(arg+3);
   printf("\n");
   */
+}
+void sub4(){
+
+}
+void sub5(void){
+
 }
